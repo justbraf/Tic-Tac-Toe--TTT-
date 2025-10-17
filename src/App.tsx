c8cc4f158b1ab3de66a9702140016ef4a8a021d6
justbraf <dwaynecorey@gmail.com>
2025-10-17 12:32:48 -0400

    added squash gui

--- src/App.tsx
+++ src/App.tsx
@@ -42,56 +42,54 @@ function App() {
   }
 
   const checkSquash = (theGrid) => {
-    for (let index = 0; index < theGrid.length; index++) { 
-    if (theGrid[index] == EMPTY)
-      return false
+    return !theGrid.includes(EMPTY) && !board.winner
   }
-  return !board.winner
-}
 
-const handleTurn = (pos) => {
-  const allBoardPositions = [...board.position]
-  allBoardPositions[pos] = board.player
-  setBoard({
-    player: board.player == CROSS ? CIRCLE : CROSS,
-    position: allBoardPositions,
-    winner: checkWinner(allBoardPositions),
-    squash: checkSquash(allBoardPositions)
-  })
-}
+  const handleTurn = (pos) => {
+    const allBoardPositions = [...board.position]
+    allBoardPositions[pos] = board.player
+    setBoard({
+      player: board.player == CROSS ? CIRCLE : CROSS,
+      position: allBoardPositions,
+      winner: checkWinner(allBoardPositions),
+      squash: checkSquash(allBoardPositions)
+    })
+  }
 
-const resetGame = () => {
-  setBoard({
-    player: CROSS,
-    position: [
-      EMPTY, EMPTY, EMPTY,
-      EMPTY, EMPTY, EMPTY,
-      EMPTY, EMPTY, EMPTY
-    ],
-    winner: false
-  })
-}
+  const resetGame = () => {
+    setBoard({
+      player: CROSS,
+      position: [
+        EMPTY, EMPTY, EMPTY,
+        EMPTY, EMPTY, EMPTY,
+        EMPTY, EMPTY, EMPTY
+      ],
+      winner: false,
+      squash: false
+    })
+  }
 
-return (
-  <div className="flex flex-col items-center justify-center h-screen gap-4">
-    <h1 className="text-4xl">Tic Tac Toe</h1>
-    {/* st grid box */}
-    <div className="grid grid-cols-3 gap-1 bg-stone-400">
-      <Square position={0} value={board.position[0]} takeTurn={handleTurn} />
-      <Square position={1} value={board.position[1]} takeTurn={handleTurn} />
-      <Square position={2} value={board.position[2]} takeTurn={handleTurn} />
-      <Square position={3} value={board.position[3]} takeTurn={handleTurn} />
-      <Square position={4} value={board.position[4]} takeTurn={handleTurn} />
-      <Square position={5} value={board.position[5]} takeTurn={handleTurn} />
-      <Square position={6} value={board.position[6]} takeTurn={handleTurn} />
-      <Square position={7} value={board.position[7]} takeTurn={handleTurn} />
-      <Square position={8} value={board.position[8]} takeTurn={handleTurn} />
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-4xl">Tic Tac Toe</h1>
+      {/* st grid box */}
+      <div className="grid grid-cols-3 gap-1 bg-stone-400">
+        <Square position={0} value={board.position[0]} takeTurn={handleTurn} />
+        <Square position={1} value={board.position[1]} takeTurn={handleTurn} />
+        <Square position={2} value={board.position[2]} takeTurn={handleTurn} />
+        <Square position={3} value={board.position[3]} takeTurn={handleTurn} />
+        <Square position={4} value={board.position[4]} takeTurn={handleTurn} />
+        <Square position={5} value={board.position[5]} takeTurn={handleTurn} />
+        <Square position={6} value={board.position[6]} takeTurn={handleTurn} />
+        <Square position={7} value={board.position[7]} takeTurn={handleTurn} />
+        <Square position={8} value={board.position[8]} takeTurn={handleTurn} />
+      </div>
+      {/* end grid box */}
+      <p className="text-xl">Player {board.player == CROSS ? "X's" : "O's"} Turn</p>
+      {board.winner && <Winner rstGame={resetGame} sqState={board.squash}/>}
+      {board.squash && <Winner rstGame={resetGame} sqState={board.squash}/>}
     </div>
-    {/* end grid box */}
-    <p className="text-xl">Player {board.player == CROSS ? "X's" : "O's"} Turn</p>
-    {board.winner && <Winner rstGame={resetGame} />}
-  </div>
-)
+  )
 }
 
 export default App
--- src/Winner.tsx
+++ src/Winner.tsx
@@ -2,10 +2,10 @@ const Winner = (props) => {
     const handleReset = () => {
         props.rstGame()
     }
-    
+
     return (
         <div className="flex flex-col items-center gap-4">
-            <h1 className="text-2xl font-bold">Winner!</h1>
+            <h1 className="text-2xl font-bold">{props.sqState?"Squashed Game!" : "Winner!"}</h1>
             <button className="border-2 rounded-full p-2 hover:bg-red-600 hover:text-white" onClick={handleReset}>Reset Game</button>
         </div>
     )
